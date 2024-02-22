c5f3f4623929c9eaa2118a2f946480587c0313f9
Tania <tanner.shirkey@gmail.com>
2024-02-22 17:54:09 -0500

    Iterates set string from user input in console, must figure out how to return multiple integers from array.

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // Grabs the input button by html id.
-var characterLimit = []; 
+
+var dictionary = { // Generated numbers use dictionary object and arrays
+  int: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+}
 
 function generatePassword() { // Function that runs when generate button is pressed
-  var length = prompt("How long should our Password be? (8-128 Limit)"); 
-  if (length >= 8 && length <= 129) { // Asks for character length, then checks if it fits within limit.
-    characterLimit.length = length;
-    return(characterLimit.length);
-  }
+  var characterLimit = prompt("How long should our Password be? (8-128 Limit)"); 
+  if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
+    for (var i = 0; i < characterLimit; i++) {
+      console.log(dictionary.int[i]); // Throws error, but does iterate correctly in console.
+    }
+  } alert("ERROR"); // Debug error alert
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();// Function is undefined, missing variable
+  var password = generatePassword();
   var passwordText = document.querySelector("#password"); //Grabs <textarea>
 
   passwordText.value = password; // Final password is a function.
