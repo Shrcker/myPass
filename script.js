3c476d21a56984155c900e5e70ebc0b5d9080adf
Tania <tanner.shirkey@gmail.com>
2024-02-22 21:51:09 -0500

    Number array now iterates and loops according to user input

--- script.js
+++ script.js
@@ -2,20 +2,23 @@
 var generateBtn = document.querySelector("#generate"); // Grabs the input button by html id.
 
 var dictionary = { // Generated numbers use dictionary object and arrays
-  int: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+  // int: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
+  int: "1 2 3 4 5 6 7 8 9 0".split(' ')
 }
 
 function generatePassword() { // Function that runs when generate button is pressed
   var characterLimit = prompt("How long should our Password be? (8-128 Limit)"); 
   if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
+    var value = '';
     for (var i = 0; i < characterLimit; i++) {
-      console.log(dictionary.int[i]); // Throws error, but does iterate correctly in console.
-    }
+      value += dictionary.int[i % dictionary.int.length]; // Loops the array
+    } return value;
   } alert("ERROR"); // Debug error alert
 }
 
 // Write password to the #password input
-function writePassword() {
+function writePassword(event) {
+  event.preventDefault(); // Prevents JavaScript from running default click event
   var password = generatePassword();
   var passwordText = document.querySelector("#password"); //Grabs <textarea>
 
@@ -25,4 +28,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword());
+generateBtn.addEventListener("click", writePassword);
