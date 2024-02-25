e31d1e015646c575ed379acf6e79adbf379ac7e9
Tania <tanner.shirkey@gmail.com>
2024-02-24 21:32:14 -0500

    Changed value to finalPass and made it an array to be able to push into it

--- script.js
+++ script.js
@@ -2,17 +2,16 @@
 var generateBtn = document.querySelector("#generate"); // Grabs the input button by html id.
 
 var dictionary = { // Generated numbers use dictionary object and arrays
-  // int: ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
   int: "1 2 3 4 5 6 7 8 9 0".split(' ')
 }
 
 function generatePassword() { // Function that runs when generate button is pressed
   var characterLimit = prompt("How long should our Password be? (8-128 Limit)"); 
   if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
-    var value = '';
+    var finalPass = []; //final Password should be an array, so that it can be pushed onto
     for (var i = 0; i < characterLimit; i++) {
-      value += dictionary.int[i % dictionary.int.length]; // Loops the array
-    } return value;
+      finalPass += dictionary.int[i % dictionary.int.length]; // Loops the array
+    } return finalPass;
   } alert("ERROR"); // Debug error alert
 }
 
