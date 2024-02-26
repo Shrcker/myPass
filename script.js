551df7036ba5b96112653e7e78bbbcb623649f80
Tania <tanner.shirkey@gmail.com>
2024-02-25 21:52:58 -0500

    Tried placing for loops into a function, but it is now returning undefined. Need to debug

--- script.js
+++ script.js
@@ -16,23 +16,23 @@ function get_random (list) {
   return list[Math.floor((Math.random()*list.length))]; // Generates a random number between 0 and 9.
 }
 
+function passGenLoop (index, limit, array) {
+  for (var i = 0; i < limit; i++) {
+    array.push(get_random(choices.charLists[index]));
+  } return array.join('');
+}
+
 function generatePassword() { // Function that runs when generate button is pressed
-  var finalPass = [];
   var characterLimit = window.prompt("How long should our Password be? (8-128 Limit)"); 
   if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
+    var finalPass = [];
     var answer = window.prompt("What kind of characters should be included? Choose from letters, numbers, or symbols (Please choose at least one).", "Numbers, Letters, or Symbols");
     if (answer.toLowerCase() === choices.charNames[0]) {
-      for (var i = 0; i < characterLimit; i++) {
-        finalPass.push(get_random(choices.charLists[0])); // Runs random number function to randomly pull from the array.
-      } return finalPass.join(''); // Numbers in generated array now display without commas or spaces.
+      passGenLoop(0, characterLimit, finalPass);
     } else if (answer.toLowerCase() === choices.charNames[1]) {
-      for (var i = 0; i < characterLimit; i++) {
-        finalPass.push(get_random(choices.charLists[1]));
-      } return finalPass.join('');
+      passGenLoop(1, characterLimit, finalPass);
     } else if (answer.toLowerCase() === choices.charNames[2]) {
-        for (var i = 0; i < characterLimit; i++) {
-          finalPass.push(get_random(choices.charLists[2]));
-        } return finalPass.join('');
+      passGenLoop(2, characterLimit, finalPass);
     }
   } alert("ERROR"); // Debug error alert
 }
