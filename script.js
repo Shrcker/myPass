a87982cc05258c865f6366bb973dd478497f31c0
Tania <tanner.shirkey@gmail.com>
2024-02-25 21:30:10 -0500

    Added functionality for other character type passwords to be generated

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ const dictionary = { // Generated numbers use dictionary object and arrays
   int: "1234567890".split(''),
   char: 'abcdefghijklmnopqrstuvwxyz'.split(''),
   special: "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~".split('')
-}
+};
+
+const choices = {
+  charNames: ["numbers", "letters", "symbols"],
+  charLists: [dictionary.int, dictionary.char, dictionary.special]
+};
 
 function get_random (list) {
   return list[Math.floor((Math.random()*list.length))]; // Generates a random number between 0 and 9.
@@ -16,10 +21,18 @@ function generatePassword() { // Function that runs when generate button is pres
   var characterLimit = window.prompt("How long should our Password be? (8-128 Limit)"); 
   if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
     var answer = window.prompt("What kind of characters should be included? Choose from letters, numbers, or symbols (Please choose at least one).", "Numbers, Letters, or Symbols");
-    if (answer.toLowerCase() === "numbers") {
+    if (answer.toLowerCase() === choices.charNames[0]) {
       for (var i = 0; i < characterLimit; i++) {
-        finalPass.push(get_random(dictionary.int)); // Runs random number function to randomly pull from the array.
+        finalPass.push(get_random(choices.charLists[0])); // Runs random number function to randomly pull from the array.
       } return finalPass.join(''); // Numbers in generated array now display without commas or spaces.
+    } else if (answer.toLowerCase() === choices.charNames[1]) {
+      for (var i = 0; i < characterLimit; i++) {
+        finalPass.push(get_random(choices.charLists[1]));
+      } return finalPass.join('');
+    } else if (answer.toLowerCase() === choices.charNames[2]) {
+        for (var i = 0; i < characterLimit; i++) {
+          finalPass.push(get_random(choices.charLists[2]));
+        } return finalPass.join('');
     }
   } alert("ERROR"); // Debug error alert
 }
