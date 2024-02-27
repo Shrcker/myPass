d787d2843506ffe3b99952e7b6b6f9d4aeec4e30
Tania <tanner.shirkey@gmail.com>
2024-02-26 22:29:24 -0500

    simplified code with help, now full functional.

--- script.js
+++ script.js
@@ -1,19 +1,14 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate"); // Grabs the input button by html id.
 
-var finalPass = [];
+const numbers = '1234567890';
+const letters = 'abcdefghijklmnopqrstuvwxyz';
+const upperCase= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const special = "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"; 
 
-const dictionary = { // Generated numbers use dictionary object and arrays
-  int: '1234567890'.split(''),
-  char: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ',
-  special: "!\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-  format: []
-};
+var pool = ''; // Empty string acting as a template for the random password.
 
-const choices = {
-  charNames: ["numbers", "letters", "symbols"],
-  charLists: [dictionary.int, dictionary.char.split(''), dictionary.special.split('')]
-};
+const finalPass = [];
 
 function get_random (list) {
   return list[Math.floor((Math.random()*list.length))];
@@ -21,70 +16,36 @@ function get_random (list) {
 
 function passGenLoop (limit) {
   for (var i = 0; i < limit; i++) {
-    finalPass.push(get_random(dictionary.format));
+    finalPass.push(get_random(pool.split('')));
   }
 }
 
 function generatePassword() { // Function that runs when generate button is pressed
-  var newDict;
+
   var characterLimit = window.prompt("How long should our Password be? (8-128 Limit)"); 
   if (characterLimit >= 8 && characterLimit <= 128) { // Asks for character length, then checks if it fits within limit.
-    var answer = window.prompt("What kind of characters should be included? Choose from letters, numbers, or symbols (Please choose at least one).", "Numbers, Letters, or Symbols");
-
-    if (answer.toLowerCase() === choices.charNames[0]) {
-
-      var addMore = window.confirm("Would you like to add another character type?");
-      writePassList(0);
-      if (addMore) {
-        addMore = window.confirm("Would you like to add letters?");
-
-        if (addMore) {
-          writePassList(1);
-          addMore = window.confirm("Would you like to add symbols too?");
-
-          if (addMore) {
-            writePassList(2);
-            passGenLoop(characterLimit);
-
-          } return finalPass.join(''); 
-        } passGenLoop(characterLimit); return finalPass.join('');
-      } passGenLoop(characterLimit); return finalPass.join('');
-
-    } else if (answer.toLowerCase() === choices.charNames[1]) {
-      var addMore = window.confirm("Would you like to add another character type?");
-      writePassList(1);
-      if (addMore) {
-        addMore = window.confirm("Would you like to add numbers?");
-        if (addMore) {
-          writePassList(0);
-          addMore = window.confirm("Would you like to add symbols too?");
-          if (addMore) {
-            writePassList(2);
-            passGenLoop(characterLimit);
-
-          } return finalPass.join(''); 
-        } passGenLoop(characterLimit); return finalPass.join(''); 
-      } passGenLoop(characterLimit); return finalPass.join(''); 
-
-    } else if (answer.toLowerCase() === choices.charNames[2]) {
-      var addMore = window.confirm("Would you like to add another character type?");
-      writePassList(2);
-      if (addMore) {
-        addMore = window.confirm("Would you like to add numbers?");
-        if (addMore) {
-          writePassList(0);
-          addMore = window.confirm("Would you like to add letters too?");
-          writePassList(1);
-          passGenLoop(characterLimit);
-
-        } return finalPass.join(''); 
-      } passGenLoop(characterLimit); return finalPass.join(''); 
-    } passGenLoop(characterLimit); return finalPass.join(''); 
-
-  } alert("ERROR"); // Debug error alert
-  function writePassList (choice) {
-    newDict = dictionary.format.concat(choices.charLists[choice]);
-}
+    var answerInt = window.confirm('Do you want this password to include numbers?');
+    if (answerInt) {
+      pool += numbers;
+    }
+    var answerLetters = window.confirm('Do you want this password to include lower case letters?');
+    if (answerLetters) {
+      pool += letters;
+    }
+    var answerCase = window.confirm('Do you want this password to include upper case letters?');
+    if (answerCase) {
+      pool += upperCase;
+    }
+    var answerSpecial = window.confirm('Do you want this password to include special characters?');
+    if (answerSpecial) {
+      pool += special;
+    } if (!answerSpecial && !answerCase && !answerLetters && !answerInt) {
+      window.alert("Please select a data type");
+      return;
+    }
+    passGenLoop(characterLimit);
+    return finalPass.join('');
+  } 
 }
 
 // Write password to the #password input
