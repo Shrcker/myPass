8d398042cd8826495a4f19b0268606780e9f6cd3
Tania <tanner.shirkey@gmail.com>
2024-04-09 16:37:24 -0400

    Refactored JS

--- script.js
+++ script.js
@@ -1,53 +1,39 @@
-var generateBtn = document.getElementById("generate"); // Grabs the input button by html id.
+const generateBtn = document.getElementById("generate"); // Grabs the input button by html id.
 
 const numbers = "1234567890";
 const letters = "abcdefghijklmnopqrstuvwxyz";
 const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const special = "!\"#$%&'()*+,-./:;<=>?@[]^_`{|}~";
 
-var pool = ""; // Empty string acting as a template for the random password.
-
-const finalPass = [];
-
-function get_random(list) {
-  return list[Math.floor(Math.random() * list.length)];
-}
-
-function passGenLoop(limit) {
-  for (var i = 0; i < limit; i++) {
-    finalPass.push(get_random(pool.split(""))); // Generation function takes the final pool string and turns it into a reference array for the final password.
-  }
-}
-
-function generatePassword() {
+const generatePassword = () => {
+  let pool = ""; // Empty string acting as a template for the random password.
+  let finalPass = [];
   // Function that runs when generate button is pressed
+  const passGenLoop = (limit, array) => {
+    for (let i = 0; i < limit; i++) {
+      array.push(getRandom(pool.split(""))); // Generation function takes the final pool string and turns it into a reference array for the final password.
+    }
+  };
+  const getRandom = (list) => {
+    return list[Math.floor(Math.random() * list.length)];
+  };
 
-  var characterLimit = window.prompt(
-    "How long should our Password be? (8-128 Limit)"
-  );
+  const characterLimit = window.prompt("How long should our Password be? (8-128 Limit)");
   if (characterLimit >= 8 && characterLimit <= 128) {
     // Asks for character length, then checks if it fits within limit.
-    var answerInt = window.confirm(
-      "Do you want this password to include numbers?"
-    ); // Will parse through ever possible data type, plugging them into the pool string before generating the password.
+    const answerInt = window.confirm("Do you want this password to include numbers?"); // Will parse through ever possible data type, plugging them into the pool string before generating the password.
     if (answerInt) {
       pool += numbers;
     }
-    var answerLetters = window.confirm(
-      "Do you want this password to include lower case letters?"
-    );
+    const answerLetters = window.confirm("Do you want this password to include lower case letters?");
     if (answerLetters) {
       pool += letters;
     }
-    var answerCase = window.confirm(
-      "Do you want this password to include upper case letters?"
-    );
+    const answerCase = window.confirm("Do you want this password to include upper case letters?");
     if (answerCase) {
       pool += upperCase;
     }
-    var answerSpecial = window.confirm(
-      "Do you want this password to include special characters?"
-    );
+    const answerSpecial = window.confirm("Do you want this password to include special characters?");
     if (answerSpecial) {
       pool += special;
     }
@@ -56,20 +42,20 @@ function generatePassword() {
       window.alert("Please select a data type");
       return;
     }
-    passGenLoop(characterLimit); // Generates full password with all selected data types added.
+    passGenLoop(characterLimit, finalPass); // Generates full password with all selected data types added.
     return finalPass.join("");
   }
   window.alert("Please enter the password's length");
   return;
-}
+};
 
 // Write password to the #password input
 function writePassword(event) {
   event.preventDefault(); // Prevents JavaScript from running default click event
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password"); //Grabs <textarea>
+  const passwordText = document.querySelector("#password"); //Grabs <textarea>
+  const password = generatePassword();
 
-  passwordText.value = password; // Final password is a function.
+  passwordText.innerText = password; // Final password is a function.
 }
 
 // Add event listener to generate button
